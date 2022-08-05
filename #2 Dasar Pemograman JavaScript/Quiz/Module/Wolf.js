12bd0d571a827574ec6cf5f1146c47349b945f23
putriliza <putriliza05@gmail.com>
2022-08-05 17:26:47 +0700

    add #2

---
+++ #2 Dasar Pemograman JavaScript/Quiz/Module/Wolf.js
@@ -0,0 +1,13 @@
+class Wolf {
+    constructor() {
+      this.strength = Math.floor(Math.random() * 100);
+    }
+  
+    howl() {
+      return 'Auuuuuuuuu';
+    }
+  }
+
+
+// export default Wolf;
+module.exports = Wolf;
